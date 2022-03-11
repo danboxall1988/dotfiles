c38a17f8f98ead7b187d1a433759bd7516a76dd6
danboxall1988 <danboxall1988@gmail.com>
2022-03-11 14:54:07 +0000

    update

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ensure at least one file is added
+if [ $# == 0 ]
+then
+    echo "You forgot to add the files!!!"
+    exit 1
+fi
+
+# ensure all files are real exist
+for file in $*
+do
+    if [ ! -f "$file" ]
+    then
+        echo "file $file does not exist!"
+        exit 2
+    fi
+done
+
+# if all files exist, add them
+for file in $*
+do
+    git add $file
+done
+
+# commit and push the files
+git commit -m 'update'
+
+git push origin main
