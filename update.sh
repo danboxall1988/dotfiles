efcd5aa8f7f126d6b70eb841d32e126c926a6546
danboxall1988 <danboxall1988@gmail.com>
2022-03-11 20:01:43 +0000

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-RED='\033[0;31m'
+G='\033[0;32m'
+R='\033[0;31m'
 NC='\033[0m' # No Color
+
 
 fails=0     #fail counter
 wins=0     #file counter
@@ -10,7 +12,7 @@ wins=0     #file counter
 # ensure at least one file is added
 if [ $# == 0 ]
 then
-    printf "\n${RED}Failed:${NC} You forgot to add the files!!!\n"
+    printf "\n${R}Failed:${NC} You forgot to add the files!!!\n"
     exit 1
 fi
 
@@ -24,7 +26,7 @@ do
     if [ ! -f "$file" ]
     then
         ((++fails))
-        printf "file ${RED}$file${NC} does not exist!\n"
+        printf "file ${R}$file${NC} does not exist!\n"
     else
         ((++wins))
     fi
@@ -35,7 +37,7 @@ printf "\n\n fails = $fails\n\n"
 # if any of the files are non-existent, exit
 if [ $fails != 0 ]
 then
-    printf "\n${RED}Failed:${NC} $fails files did not exist!\n"
+    printf "\n${R}Failed:${NC} $fails files did not exist!\n"
     exit 2
 fi
 
@@ -52,4 +54,4 @@ git commit -m 'update'
 git push origin main
 
 
-printf "\n${RED}SUCCESS!${NC} $wins files updated\n"
+printf "\n${G}SUCCESS!${NC} $wins files updated\n"
